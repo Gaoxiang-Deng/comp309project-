003901527d368eb24ce2dd762287e3e2ee8b91e5
Gaoxiang-Deng <61376742+Gaoxiang-Deng@users.noreply.github.com>
2021-10-24 16:04:36 +1300

    Update read_image.py

--- read_image.py
+++ read_image.py
@@ -6,11 +6,19 @@ from torch import classes
 from torchvision import transforms
 
 transform = transforms.Compose(
-    [transforms.ToTensor(),
+    [transforms.RandomResizedCrop(224),
+     transforms.RandomHorizontalFlip(),
+     transforms.Resize([32, 32]),
+     transforms.ToTensor(),
      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 transform_test = transforms.Compose(
-    [transforms.ToTensor(),
+    [transforms.RandomResizedCrop(224),
+     transforms.RandomHorizontalFlip(),
+     transforms.Resize([32, 32]),
+     transforms.ToTensor(),
      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+classes = ('cherry', 'strawberry', 'tomato')
 
 data_path = "C:/workspace/traindata/traindata"
 data_all = torchvision.datasets.ImageFolder(root=data_path, transform=transform)
@@ -38,10 +46,18 @@ def dataset_sampler(dataset, val_percentage=0.1):
 
 train_sampler, val_sampler = dataset_sampler(data_all)
 
-batch_size = 4
+batch_size = 20
 trainloader = data.DataLoader(data_all, batch_size=batch_size, num_workers=0, sampler=train_sampler)
 testloader = data.DataLoader(data_all, batch_size=batch_size, num_workers=0, sampler=val_sampler)
 
+##获得一个batch的数据
+for step, (b_x, b_y) in enumerate(trainloader):
+    if step > 0:
+        break
+
+print(b_x.shape)
+print(b_y.shape)
+print("图像的取值范围为：", b_x.min(), "~", b_x.max())
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
@@ -50,7 +66,7 @@ import numpy as np
 def imshow(img):
     img = img / 2 + 0.5
     npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.imshow(np.transpose(npimg, (1, 2, 0)), vmin=0, vmax=255)
     plt.show()
 
 
@@ -58,7 +74,8 @@ dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
 imshow(torchvision.utils.make_grid(images))
-print(''.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+print(classes)
+print(','.join('%5s' % classes[labels[j]] for j in range(batch_size)))
 
 # %%
 import torch.nn as nn
@@ -68,9 +85,9 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 4, 300)
+        self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(3, 4, 300)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
@@ -78,7 +95,7 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -86,17 +103,16 @@ class Net(nn.Module):
 
 
 import torch.optim as optim
+
 net = Net()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-
+# %%
 for epoch in range(2):
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
         optimizer.zero_grad()
-        print(len(inputs))
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -107,11 +123,6 @@ for epoch in range(2):
             running_loss = 0.0
 
 print('Finish training')
-
-
-# %%
-
-
 
 # %%
 PATH = './cifar_net.pth'
@@ -130,3 +141,58 @@ net.load_state_dict(torch.load(PATH))
 
 # %%
 outputs = net(image)
+
+# %%
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
+
+# %%
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
+
+# %%
+correct_pred = {classname: 0 for classname in classes}
+total_pred = {classname: 0 for classname in classes}
+
+# again no gradients needed
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predictions = torch.max(outputs, 1)
+        # collect the correct predictions for each class
+        for label, prediction in zip(labels, predictions):
+            if label == prediction:
+                correct_pred[classes[label]] += 1
+            total_pred[classes[label]] += 1
+
+# print accuracy for each class
+for classname, correct_count in correct_pred.items():
+    accuracy = 100 * float(correct_count) / total_pred[classname]
+    print("Accuracy for class {:5s} is: {:.1f} %".format(classname,
+                                                         accuracy))
+
+# %%
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+
+print(device)
+
+# %%
+inputs, labels = data[0].to(device), data[1].to(device)
